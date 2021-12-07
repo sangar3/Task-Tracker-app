5d870a565919af7cbdb4af0c7a19b8cdf1f34f72
sangar3 <55110746+sangar3@users.noreply.github.com>
2021-12-07 15:54:33 -0600

    front end added

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, {FC, useState} from 'react'; //add FC = functional component   type
+import "./App.css"
 
-function App() {
+// app is a functional component 
+const App:FC = () =>  {
+
+  
+
   return (
     <div className="App">
-     
+
+      <div className="header">
+          <div className="inputContainer">
+            <input type="text" placeholder="Task.."></input>
+            <input type="number" placeholder="Deadline (DAYS).."></input>
+          </div>
+          <button>Add Task</button>
+      </div>
+
+      <div className="todoList">
+
+      </div>
+
     </div>
   );
 }
